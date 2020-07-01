5e376eefdfe7ee99e602424ae976b05e164aae8b
Jono Hewitt <jonohewitt@gmail.com>
2020-07-01 18:55:04 +0100

    Update immediately on load

--- scroll.js
+++ scroll.js
@@ -1,6 +1,12 @@
 const pixelsTag = document.querySelector("div.pixels");
 
-document.addEventListener("scroll", () => {
+const tagUpdate = () => {
   const pixels = window.pageYOffset;
   pixelsTag.innerHTML = `Scrolled pixels: ${pixels}`;
+}
+
+tagUpdate();
+
+document.addEventListener("scroll", () => {
+  tagUpdate();
 });
